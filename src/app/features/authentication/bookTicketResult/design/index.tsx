785682820e5b6a1d107c6ac058424f819be5f392
thiendangit <thiendangit1102@gmail.com>
2020-12-11 09:02:36 +0700

    done button add corn & Beverages

---
+++ src/app/features/authentication/bookTicketResult/design/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {images, ImageTypes} from '@assets/image'
+import {styles} from './style'
+import {Block, Button, Form, Img, Screen, Text, Wallpaper} from "@components";
+import {useTranslation} from 'react-i18next';
+import {StyleProp} from "react-native";
+import {ColorsCustom} from "@theme/color";
+import {NavigationService, navigationRef} from "@navigation/navigationService";
+import {APP_SCREEN} from "@navigation/screenTypes";
+import {Constants, dispatch, verticalScale} from "@common";
+import {onSetAppProfile} from "@app_redux/reducer";
+import {useSelector} from "react-redux";
+
+export interface Props {
+    route: {
+        params: {
+            text: string
+        }
+    },
+}
+
+export const registerDoneScreen: React.FC<Props> = (props): React.ReactElement => {
+    const [t] = useTranslation();
+    const userType = useSelector(
+        (state: any) => state.app?.profile.user_type
+    );
+    let text = props.route.params?.text ?? '';
+
+    const onPressButtonHome = () => {
+        NavigationService.pop(2)
+    };
+
+    return (
+        <Block style={styles.container}>
+            <Img style={[styles.imageContainer]}
+                 source={images.checkDone}
+                 tintColor={userType === Constants.ROLE.SUPPLIER ? ColorsCustom.lime_green : ColorsCustom.light_red}
+                 resizeMode={"contain"}/>
+            <Text style={styles.nameSupplierBuyer}>
+                {t('common:success')}
+            </Text>
+            <Text style={[styles.text, {marginTop: verticalScale(10)}]}>
+                {t('common:congratulation')}!
+            </Text>
+            <Text style={[styles.text, {marginTop: verticalScale(5)}]}>
+                {text}
+            </Text>
+            <Button
+                onPress={onPressButtonHome}
+                style={[styles.buttonLogin, {backgroundColor: userType === Constants.ROLE.SUPPLIER ? ColorsCustom.lime_green : ColorsCustom.light_red}]}>
+                <Text style={styles.textButton}>
+                    {t('common:Home')}
+                </Text>
+            </Button>
+        </Block>
+    )
+};
---
+++ src/app/features/authentication/bookTicketResult/design/style.ts
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+import {AppTheme} from '@config/type';
+import {deviceHeight, deviceWidth} from "@utils";
+import {moderateScale, scale, verticalScale} from "@common";
+import fonts from "react-native-paper/lib/typescript/src/styles/fonts";
+import {FontSizeDefault} from "@theme/fontSize";
+import {ColorsCustom} from "@theme/color";
+
+export const styles = StyleSheet.create({
+    text: {
+        color: ColorsCustom.grey,
+        width : deviceWidth/1.3,
+        textAlign : 'center'
+    },
+    container: {
+        flex: 1,
+        alignItems : 'center',
+        backgroundColor : 'white'
+    },
+    imageContainer: {
+        marginTop: moderateScale(105),
+        width: scale(100),
+        height: scale(100),
+    },
+
+    imageSupplier_Buyer: {
+        width: scale(deviceWidth * 0.13),
+        height: scale(deviceWidth * 0.13),
+        // backgroundColor: 'red'
+    },
+
+    buttonContainer: {
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        width : deviceWidth * 0.8,
+        marginTop : verticalScale(100)
+    },
+    buttonSupplier_Buyer : {
+        backgroundColor : 'white',
+        borderWidth : 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width : scale(deviceWidth * 0.3),
+        height : scale(deviceWidth * 0.3),
+        borderRadius : scale(deviceWidth * 0.3)/10
+    },
+    nameSupplierBuyer : {
+        marginTop : moderateScale(40),
+        fontSize : FontSizeDefault.FONT_22,
+        fontWeight : 'bold'
+    },
+    buttonLogin: {
+        backgroundColor: ColorsCustom.light_red,
+        width: deviceWidth - scale(50),
+        height: scale(48),
+        position : 'absolute',
+        bottom : verticalScale(40),
+        alignSelf: 'center'
+    },
+    textButton: {
+        color: ColorsCustom.lightWhite,
+        fontSize: FontSizeDefault.FONT_16
+    },
+});
